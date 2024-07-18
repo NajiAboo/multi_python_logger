b6f8c4f935fe127afeedfdabf69c480d8b9e8a04
Naji Aboo <aboonaji@gmail.com>
2024-07-18 14:48:41 +0530

    updaed the setup with installation packages

--- setup.py
+++ setup.py
@@ -10,4 +10,5 @@ setup(
     description="Application to provide different logging mechanisms",
     keywords="python logger cloud_logger, aws_cloudwatch_logger",
     url="",
+    install_requires= ["watchtower==3.2.0", "boto3==1.34.144", "python-dotenv"],
 )
