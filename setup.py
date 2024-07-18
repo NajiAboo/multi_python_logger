714c7ee20dc10338b649e0324320910a47018fea
Naji Aboo <aboonaji@gmail.com>
2024-07-18 15:09:53 +0530

    fixes on the build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="multi_python_logger",
-    version="1.2",
+    version="2.0.0",
     packages=find_packages(),
     # Metadata
     author="Mohamed Naji Aboo",
