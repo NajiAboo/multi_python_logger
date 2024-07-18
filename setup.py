fb5d798f983681595fba3b228606ab554eee7ca3
Naji Aboo <aboonaji@gmail.com>
2024-07-18 15:27:36 +0530

    updated the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="multi_python_logger",
-    version="2.0.1",
+    version="2.0.2",
     packages=find_packages(),
     # Metadata
     author="Mohamed Naji Aboo",
