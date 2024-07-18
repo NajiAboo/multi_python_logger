2b39a7e739813bdb6b9216d3ee7b399df9d8ddda
Naji Aboo <aboonaji@gmail.com>
2024-07-18 15:21:58 +0530

    added install pckages

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="multi_python_logger",
-    version="2.0.0",
+    version="2.0.1",
     packages=find_packages(),
     # Metadata
     author="Mohamed Naji Aboo",
     author_email="aboonaji@gmail.com",
     description="Application to provide different logging mechanisms",
     keywords="python logger cloud_logger, aws_cloudwatch_logger",
-    url=""    
+    url="",
+    install_requires=["watchtower==3.2.0", "boto3==1.34.144","python-dotenv" ]
+
 )
