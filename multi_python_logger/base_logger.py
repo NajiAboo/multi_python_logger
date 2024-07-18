73e36e08f4338a7db464b6f5b75e42e855f578e5
Naji Aboo <aboonaji@gmail.com>
2024-07-18 17:15:55 +0530

    Added module name and error code

--- multi_python_logger/base_logger.py
+++ multi_python_logger/base_logger.py
@@ -7,8 +7,11 @@ class BaseLogger:
         self.logger = logging.getLogger(self.__class__.__name__)
         self.logger.setLevel(logging.DEBUG)
         self.handler = self.create_handler()
+        # formatter = logging.Formatter(
+        #     "%(asctime)s - %(name)s - %(levelname)s - %(message)s - [%(filename)s:%(lineno)d]"
+        # )
         formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s - [%(filename)s:%(lineno)d]"
+            '%(asctime)s - %(name)s - %(filename)s:%(lineno)d - %(module_name)s - %(error_code)s - %(levelname)s - %(message)s'
         )
         self.handler.setFormatter(formatter)
         self.logger.addHandler(self.handler)
@@ -18,3 +21,13 @@ class BaseLogger:
 
     def get_logger(self):
         return self.logger
+
+
+
+class CustomLogRecord(logging.LogRecord):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.module_name = kwargs.get('module_name', '')
+        self.error_code = kwargs.get('error_code', '')
+
+logging.setLogRecordFactory(CustomLogRecord)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="multi_python_logger",
-    version="2.0.3",
+    version="2.0.4",
     packages=find_packages(),
     # Metadata
     author="Mohamed Naji Aboo",
