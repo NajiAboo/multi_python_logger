981fd715de9bee816d945c7adf7bbb1fdcfcfd88
Naji Aboo <aboonaji@gmail.com>
2024-07-18 12:31:22 +0530

    created basic configuration files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Multi Python Logger",
+    version="1.0",
+    packages=find_packages(),
+    # Metadata
+    author="Mohamed Naji Aboo",
+    author_email="aboonaji@gmail.com",
+    description="Application to provide different logging mechanisms",
+    keywords="python logger cloud_logger, aws_cloudwatch_logger",
+    url="",
+)
